a05949dc4bf1672e50d41bfff8f9ea05645c8fab
Nishu1111 <nishunicks1113@gmail.com>
2025-07-13 18:12:02 +0530

    solve error

--- smartrate-frontend/src/App.js
+++ smartrate-frontend/src/App.js
@@ -1,36 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React from 'react'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
+import './index.css';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-import Register from './components/Register';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
 }
 
+
 export default App;
-
-
-
-// function App() {
-//   return (
-//     <div className="text-4xl text-blue-600 font-bold p-4">
-//       ✅ Tailwind is working perfectly!
-//     </div>
-//   );
-// }
-
-// export default App;
-
